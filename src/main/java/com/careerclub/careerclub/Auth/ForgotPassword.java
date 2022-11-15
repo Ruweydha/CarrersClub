327e4813869783ed516100fc95c8f3472eb13e1d
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-11-15 09:39:01 +0300

    Change password request

--- src/main/java/com/careerclub/careerclub/Auth/ForgotPassword.java
+++ src/main/java/com/careerclub/careerclub/Auth/ForgotPassword.java
@@ -1,2 +1,22 @@
-package com.careerclub.careerclub.Auth;public class ForgotPassword {
+package com.careerclub.careerclub.Auth;
+
+public class ForgotPassword {
+    private String email;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
