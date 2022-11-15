3d0983eba0a58ee197e9e967ccacdb5dc1ccb68f
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-11-15 09:39:44 +0300

    Created the view for changing your password

--- src/main/java/com/careerclub/careerclub/Controllers/changePasswordController.java
+++ src/main/java/com/careerclub/careerclub/Controllers/changePasswordController.java
@@ -1,2 +1,25 @@
-package com.careerclub.careerclub.Controllers;public class changePasswordController {
+package com.careerclub.careerclub.Controllers;
+
+import com.careerclub.careerclub.DTOs.ChangePasswordDto;
+import com.careerclub.careerclub.Service.ChangePasswordService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/password/change")
+@Tag(name ="Change password controller")
+public class changePasswordController {
+    private final ChangePasswordService changePasswordService;
+
+    public changePasswordController(ChangePasswordService changePasswordService) {
+        this.changePasswordService = changePasswordService;
+    }
+
+    @PostMapping
+    public String changePassword(ChangePasswordDto changePasswordDto){
+        changePasswordService.changeYourPassword(changePasswordDto);
+        return "Password changed successfully";
+    }
 }
