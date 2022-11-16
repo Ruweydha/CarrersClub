bdfa753ec37921acfdd2d11d1b68dd177260ebc2
Eugene-Oluoch <eugenemarkke@gmail.com>
2022-11-16 10:16:38 +0300

    Created a saved job controller

--- src/main/java/com/careerclub/careerclub/SavedJobs/controller/SavedJobsController.java
+++ src/main/java/com/careerclub/careerclub/SavedJobs/controller/SavedJobsController.java
@@ -1,2 +1,38 @@
-package com.careerclub.careerclub.SavedJobs.controller;public class SavedJobsController {
+package com.careerclub.careerclub.SavedJobs.controller;
+
+
+import com.careerclub.careerclub.DTOs.SaveJobRequest;
+import com.careerclub.careerclub.SavedJobs.service.SavedJobService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("save-job")
+public class SavedJobsController {
+
+    private final SavedJobService savedJobService;
+
+    public SavedJobsController(SavedJobService savedJobService) {
+        this.savedJobService = savedJobService;
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllUserSavedJobs(){
+        var jobs = savedJobService.getAllUserSavedJobs();
+        return ResponseEntity.ok(jobs);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveJob(@RequestBody SaveJobRequest saveJobRequest){
+        var savedJob = savedJobService.saveJob(saveJobRequest.getJobId());
+        return ResponseEntity.ok(savedJob);
+    }
+
+    @DeleteMapping("/unsave/{jobId}")
+    public ResponseEntity<?> unSaveJob(@PathVariable Long jobId){
+        var message = savedJobService.unSaveJob(jobId);
+        return ResponseEntity.ok(message);
+    }
+
 }
